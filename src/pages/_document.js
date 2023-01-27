088365ff3535ad184587c76b6e26b7e683439dfc
jahan <jahan.pd@gmail.com>
2023-01-27 21:18:50 +1100

    padding base for safari

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <body className="min-h-screen">
+      <body className="min-h-screen pb-safe">
         <Main />
         <NextScript />
       </body>
